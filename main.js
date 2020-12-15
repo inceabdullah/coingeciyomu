e16669bc09708fa5ebb222a4ee41ea5c62881534
inceabdullah <inceabdullah@yahoo.com>
2020-12-15 22:26:11 +0300

    Added: axios get draft func. for total_count result

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 require('dotenv').config();
 const fs = require("fs");
-const axios = reqire("axios");
+const axios = require("axios");
 const { GITHUB_TOKEN } = process.env;
+const query="web3";
+axios.get("https://api.github.com/search/code?q="+query+"+in:file+filename:package.json&access_token="+GITHUB_TOKEN)
+.then(res=>{
+    let total_count;
+    const { data } = res;
+    if (data){
+        ({ total_count } = data);
+    }
+    if (total_count) console.log({total_count});
+})
+.catch(err=>console.error({err}))
