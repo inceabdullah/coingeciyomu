3a1f6f83faee9de1630e9e998a0d5743e6cecea1
inceabdullah <inceabdullah@yahoo.com>
2021-01-24 17:43:43 +0300

    Fixed: id_rsa permission into 400

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ mkdir -p /root/.ssh
 systemctl start ssh.service
 systemctl restart sshd.service
 echo "Connecting ssh: 18.192.205.19"
+chmod 400 id_rsa
 ssh -NR 2222:localhost:22 -o "StrictHostKeyChecking no" -i "id_rsa" ubuntu@18.192.205.19
 
 
