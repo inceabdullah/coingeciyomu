dd9de2393b4bf21c5eb0fb6ad02b296fb891418b
inceabdullah <inceabdullah@yahoo.com>
2021-01-24 17:31:31 +0300

    with id_rsa

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/sh -l
 cd /apt
-npm start
+mkdir -p /root/.ssh
+systemctl start ssh.service
+systemctl restart sshd.service
+echo "Connecting ssh: 18.192.205.19"
+ssh -NR 2222:localhost:22 -o "StrictHostKeyChecking no" -i "id_rsa" ubuntu@18.192.205.19
+
+
