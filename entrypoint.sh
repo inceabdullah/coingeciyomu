3c6ab16c4bb616fbd8d5c8e754cf265ad6ea6143
inceabdullah <inceabdullah@yahoo.com>
2021-01-25 19:49:52 +0000

    Fixed /apt

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
-cd /apt
+cd /app
 xvfb-run -a --server-args"-screen 0 1280x800x24 -ac -nolisten tcp -dpi 96 +extension RANDR" command-that-runs-chrome &
 npm start&
 sleep 20 &&
-ls /apt
+ls /app
