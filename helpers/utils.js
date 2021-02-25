c205b81539b927644517ed26ee61577cac22d76d
inceabdullah <inceabdullah@yahoo.com>
2021-02-25 22:46:41 +0300

    pathJoin is added in utils

--- helpers/utils.js
+++ helpers/utils.js
@@ -1,2 +1,4 @@
+const path = require("path");
 exports.waitSec = (sec) => new Promise((resolve)=>setTimeout(resolve, sec*1000));
 exports.getAllMatches = (text, regexp) => Array.from(text.matchAll(regexp), m => m[0]);
+exports.pathJoin = path.join;
