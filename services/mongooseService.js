b006ec55857be0c8487c8f3fb1d7a0c1baba4d06
inceabdullah <inceabdullah@yahoo.com>
2021-02-27 09:40:48 +0300

    updateLinkedInCookies is added in mongoose services

--- services/mongooseService.js
+++ services/mongooseService.js
@@ -16,3 +16,22 @@ exports.getLinkedInCookies = () => new Promise((resolve, reject)=>{
         resolve(cookieJSON);
     });
 });
+
+exports.updateLinkedInCookies = (cookies) => new Promise((resolve, reject)=>{
+    if (!mongoose) return reject("MongoDB is not loaded.");
+    const cookieSchema = new mongoose.Schema({
+        name: String,
+        stringify: String
+    });
+    const cookieModel = mongoose.model("Cookie", cookieSchema);
+    const updated = await cookieModel.updateOne({name: "linkedin"}, {
+        $set: {
+            stringify: JSON.stringify(cookies, null, 2)
+        }
+    }).catch(err=>{
+        console.error({err});
+        reject(err);
+    });
+    if (!updated) return;
+    resolve(updated);
+});
