1e15725d6dba143b5a5ac043cf8bb4dfbbd0fbf2
inceabdullah <inceabdullah@yahoo.com>
2021-02-27 20:03:52 +0300

    try-12 cron

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@ cd /app
 npm install
 xvfb-run -a --server-args"-screen 0 1280x800x24 -ac -nolisten tcp -dpi 96 +extension RANDR" command-that-runs-chrome &
 npm start
-sleep 600 &&
+sleep 1200 &&
 ls /app
 
