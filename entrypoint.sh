6a0f121c5a1c3073d3c516fd04fd7dbeec631da8
inceabdullah <inceabdullah@yahoo.com>
2021-02-27 21:21:39 +0300

    add. source bashrc

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-# source /root/.bashrc
+source /root/.bashrc
 cd /app
 npm install
 xvfb-run -a --server-args"-screen 0 1280x800x24 -ac -nolisten tcp -dpi 96 +extension RANDR" command-that-runs-chrome &
