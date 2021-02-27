2ea4c371b241cc8b4f48836f50f76475fc4ea80a
inceabdullah <inceabdullah@yahoo.com>
2021-02-27 14:23:30 +0300

    & is removed end of the command line: npm run

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 cd /app
 npm install
 xvfb-run -a --server-args"-screen 0 1280x800x24 -ac -nolisten tcp -dpi 96 +extension RANDR" command-that-runs-chrome &
-npm start&
+npm start
 sleep 20 &&
 ls /app
 
