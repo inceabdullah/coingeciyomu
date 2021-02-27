62a4301e816b07d8c17c63301eea336b66eae73d
inceabdullah <inceabdullah@yahoo.com>
2021-02-27 18:02:54 +0300

    up to 10m

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@ cd /app
 npm install
 xvfb-run -a --server-args"-screen 0 1280x800x24 -ac -nolisten tcp -dpi 96 +extension RANDR" command-that-runs-chrome &
 npm start
-sleep 20 &&
+sleep 600 &&
 ls /app
 
