667a450958f18d378b2fbee2fbeb663577cc4239
inceabdullah <inceabdullah@yahoo.com>
2021-02-27 12:39:24 +0300

    Done main

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const { utils } = require("./helpers/");
+const { mongooseService } = require("./services/");
 const fs = require("fs");
 const axios = require("axios");
 const Scraper = require("./linkedInScraper");
@@ -9,6 +10,12 @@ const counts = {
     github: 0,
     linkedIn: 0
 }
+console.log({
+    C1:{
+        github: "web3 in package.json",
+        linkedin: "web3 nodejs"
+    }
+})
 const getGithub = async () => {
     const query="web3";
     const result = await axios.get("https://api.github.com/search/code?q="+query+"+in:file+filename:package.json",{
@@ -31,13 +38,12 @@ const getLinkedIn = async () => {
     await waitSec(5);
     Scraper.setQueryOptions({query: "ethereum web3"})
     await Scraper.linkedInJobSearch();
-    await waitSec(5);
-    await Scraper.getLogin().catch(err=>{
-        console.log("Error in Scraper.getLogin()");
-        console.log(err)
-    });
+    // await waitSec(5);
+    // await Scraper.getLogin().catch(err=>{
+    //     console.log("Error in Scraper.getLogin()");
+    //     console.log(err)
+    // });
     await waitSec(10);
-    await Scraper.linkedInJobSearch();
     await utils.waitSec(5);
     const count = parseInt((await Scraper.getJobCount()).replace(/\,|\+$/g, ""));
     counts.linkedIn = count;
@@ -52,7 +58,19 @@ const getCounts = async () => {
 }
 
 getCounts()
-.then(res=>console.log({res}))
+.then(res=>{
+    console.log({res});
+    const {
+        github: githubCount,
+        linkedIn: linkedinCount
+    } = res;
+    mongooseService.postC1Logs(githubCount, linkedinCount).then(res=>{
+        console.log({postC1Logs_res: res});
+    })
+    .catch(err=>{
+        console.error({postC1Logs_err: err});
+    });
+})
 .catch(err=>console.error({err}));
 
 // const puppeteer = require('puppeteer')
