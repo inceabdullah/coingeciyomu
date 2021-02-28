03bcc90fcc3f2500304731e762f20d07c9f6f3e0
inceabdullah <inceabdullah@yahoo.com>
2021-02-28 12:38:41 +0300

    mins. set. main.yml set to 6

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ cd /app
 npm install
 xvfb-run -a --server-args"-screen 0 1280x800x24 -ac -nolisten tcp -dpi 96 +extension RANDR" command-that-runs-chrome &
 npm start
-sleep 6000 &&
+sleep 600 &&
 ls /app
 
